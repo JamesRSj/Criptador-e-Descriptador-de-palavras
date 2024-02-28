c0bc2f99e971c2771e150afb48a2fd20e23f77ac
JamesRSj <159602625+JamesRSj@users.noreply.github.com>
2024-02-28 18:28:08 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,116 +1,24 @@
-const btnCrip = document.querySelector(".btn");
-const btnCopy = document.querySelector(".btn-copiar");
+var textInput = document.querySelector("#input-texto");
+var outInput = document.querySelector("#output");
 
-function criptografarMensagem() {
-  const mensagem = document.querySelector(".insercao-mensagem");
-  const mensagemOriginal = mensagem.value;
-  if (verificarCaracteres(mensagemOriginal)) {
-    alert("🚫 Não são permitidas letras maiúsculas e acentuadas! 🚫");
-    return;
-  }
-  if (mensagemOriginal.trim() === "") {
-    return;
-  }
+function criptografar(){
 
-  function substituirVogais(letra) {
-    switch (letra) {
-      case "a":
-        return "ai";
-      case "e":
-        return "enter";
-      case "i":
-        return "imes";
-      case "o":
-        return "ober";
-      case "u":
-        return "ufat";
-      default:
-        return letra;
-    }
-  }
+  var texto = textInput.value;
 
-  const mensagemCriptografada = mensagemOriginal
-    .split("")
-    .map(substituirVogais)
-    .join("");
-  atualizaResultado(mensagemCriptografada);
+  var resultCripto = texto.replace(/e/, "enter").replace(/i/, "imes").replace(/a/, "ai").replace(/o/, "ober").replace(/u/, "ufat")
+
+
+  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto + 
+  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function descriptografarMensagem() {
-  const mensagem = document.querySelector(".insercao-mensagem");
-  const mensagemCriptografada = mensagem.value;
-  if (verificarCaracteres(mensagemCriptografada)) {
-    alert("🚫 Não são permitidas letras maiúsculas e acentuadas! 🚫");
-    return;
-  }
-  const mapeamentoDescriptografar = {
-    ai: "a",
-    enter: "e",
-    imes: "i",
-    ober: "o",
-    ufat: "u",
-  };
+function descriptografar(){
 
-  function descriptografarSequencia(sequencia) {
-    return mapeamentoDescriptografar[sequencia] || sequencia;
-  }
+    var texto = textInput.value;
 
-  const sequenciasCriptografadas = mensagemCriptografada.match(
-    /(ai|enter|imes|ober|ufat)|./gi
-  );
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 
-  const mensagemDescriptografada = sequenciasCriptografadas
-    .map(descriptografarSequencia)
-    .join("");
-
-  return atualizaResultado(mensagemDescriptografada);
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function atualizaResultado(resultado) {
-  const saidaTexto = document.querySelector(".saida-texto");
-  saidaTexto.textContent = resultado;
-}
-
-function copiarMensagem() {
-  const saidaTexto = document.querySelector(".saida-texto");
-  const textoParaCopiar = saidaTexto.textContent;
-
-  navigator.clipboard
-    .writeText(textoParaCopiar)
-    .then(() => {})
-    .catch((error) => {
-      console.error(`Erro ao copiar o texto: ${error}`);
-    });
-  showToast("Texto copiado 🚀");
-  limparCampo();
-}
-
-function limparCampo() {
-  const mensagemCrip = document.querySelector(".insercao-mensagem");
-  const saidaTexto = document.querySelector(".saida-texto");
-
-  mensagemCrip.value = "";
-  saidaTexto.textContent = "";
-}
-
-function verificarCaracteres(texto) {
-  const regexCaractereEspecial = /[A-Z]/;
-  const regexAcentuacao = /[áàãâéèêíïóôõöúüç]/i;
-  return regexCaractereEspecial.test(texto) || regexAcentuacao.test(texto);
-}
-
-function showToast(message) {
-  const toast = document.createElement("div");
-  toast.className = "toast";
-  toast.textContent = message;
-  document.body.appendChild(toast);
-
-  toast.classList.add("show");
-
-  setTimeout(() => {
-    toast.classList.remove("show");
-    setTimeout(() => {
-      document.body.removeChild(toast);
-    }, 500);
-  }, 3000);
-}
